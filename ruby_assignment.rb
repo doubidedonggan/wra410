96cbcfa47f3ec6121b48554450713e0f4860d077
John Monberg <john.monberg@gmail.com>
2019-02-04 20:30:11 -0500

    Add Ruby assignment

---
+++ ruby_assignment.rb
@@ -0,0 +1,49 @@
+# Ruby Assignment
+# 
+# This assignment contains all of the requirements for the Ruby Assignment.
+#
+# Download this file.
+#
+# Make the changes in the file you need to complete the assignment in each section of this file.
+# The assignment is worth worth up to 100 points.
+#
+# Save your file and attach it to an email sent to jmonberg@msu.edu 
+# Use "WRA 410 Ruby Assignment" as the title of your email.
+#
+# When Dr. Monberg runs a copy of your ruby file, it should display all of the required elements. 
+#
+# Section 1 worth 20 points
+# 
+# Verify with Dr. Monberg that either Ruby 2.2.2 is installed on your computer or you have setup an account
+# on a server like Cloud9 https://medium.com/@krissanawat/initial-setup-enviroment-430f11200b4a
+#
+# Section 2 worth 20 points
+#
+# 1. Create two string variables for your first name and last name.
+# 2. Display your name.
+# 3. Display your name with your middle name interpolated in the middle of your name output string.
+# 4. Display your name in all capitals.
+# 5. Display the number of characters in your name.
+# 6. Find another method of the String Class and display the results of that method for your name string.
+#
+# Section 3 worth 20 points
+#
+# 1. Create an array with a list of the titles of your 5 favorite movies
+# 2. Display the first element of the array.
+# 3. Display all of the titles sorted in alphabetical order
+#
+# Section 4 worth 20 points
+#
+# Meals are important and there are a lot of details to keep track of for a meal.
+#
+# 1. Create a Meal Class. 
+# 2. Have at least 5 variables, with at least one string, integer and boolean type in your Meal class.
+# 3. Create a method for the class in addition to the initialize method
+# 4. Create and dsiplay three instances of meals.
+# 5. Call your method on all three instances.
+#
+# Section 5 
+# What is your favorite book, website, youtube series, forum, or tutorial for learning Ruby that you have
+# discovered in addition to the course materials?
+# Display the title and link of your favorite resource.
+# Display two sentences that explain why you found that resource useful.
